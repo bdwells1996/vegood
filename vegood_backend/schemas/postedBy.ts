3b1bacbb207284615abcf19dfe6ac246d641b153
Ben <bdwells1996@gmail.com>
2026-02-27 10:23:45 +0000

    Feat: Cleared out old code and set up new expo project.

--- vegood_backend/schemas/postedBy.ts
+++
@@ -1,8 +0,0 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
-    name: 'postedBy',
-    title: 'PostedBy',
-    type: 'reference',
-    to: [{ type: 'user' }],
-  })
